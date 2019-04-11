d069695eac5cdcac7611540e507820e395fcb79c
Jeffrey Corcoran <jeff.corcoran@gmail.com>
2019-04-11 10:54:53 -0400

    Made a simple but effective speed improvement

--- LongestWord/Program.cs
+++ LongestWord/Program.cs
@@ -1,69 +1,88 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LongestWord
 {
     class Program
     {
+        private static string[] _lines;
+        private static string _biggest = string.Empty;
+
         static void Main(string[] args)
         {
-            string[] lines;
-
             Console.Write("Run in Parallel? (Y/N):");
             string parallel = Console.ReadLine();
 
-            if (args.Length > 0)
-            {
-                lines = File.ReadAllLines(args[0]);
-            }
-            else
-            {
-                lines = File.ReadAllLines("wikipediaaa.txt");
-            }
-             
-            string biggest = string.Empty;
-
+            _lines = FindFile(args);
+            
             Stopwatch sw = new Stopwatch();
 
             if (parallel.ToUpper() == "Y")
-            {                
+            {
                 sw.Start();
-
-                Parallel.ForEach(lines, line =>
-                {
-                    foreach (var word in line.Split())
-                    {
-                        if (word.Length > biggest.Length)
-                        {
-                            biggest = word;
-                        }
-                    }
-                });
+                RunParallelThreads();
             }
             else
             {
                 sw.Start();
-
-                foreach (var line in lines)
-                {
-                    foreach (var word in line.Split())
-                    {
-                        if (word.Length > biggest.Length)
-                        {
-                            biggest = word;
-                        }
-                    }
-                }
+                RunSingleThread();
             }
 
             sw.Stop();
 
-            Console.WriteLine($"Longest Word: {biggest}");
-            Console.WriteLine($"Longest Word Length: {biggest.Length}");
+            Console.WriteLine($"Longest Word: {_biggest}");
+            Console.WriteLine($"Longest Word Length: {_biggest.Length}");
             Console.WriteLine($"Time Searching: {sw.ElapsedMilliseconds} ms");
             Console.ReadKey();
+        }
+
+        static string[] FindFile(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                return File.ReadAllLines(args[0]);
+            }
+            else
+            {
+                return File.ReadAllLines("wikipediaaa.txt");
+            }
+        }
+
+        static void RunSingleThread()
+        {
+            foreach (var line in _lines)
+            {
+                if (_biggest.Length > line.Length)
+                    continue;
+
+                foreach (var word in line.Split())
+                {
+                    if (word.Length > _biggest.Length)
+                    {
+                        _biggest = word;
+                    }
+                }
+            }
+        }
+
+        static void RunParallelThreads()
+        {
+            Parallel.ForEach(_lines.Where(x => x.Length > _biggest.Length), line =>
+            {
+                if (_biggest.Length > line.Length)
+                    return;
+
+                foreach (var word in line.Split())
+                {
+                    if (word.Length > _biggest.Length)
+                    {
+                        _biggest = word;
+                    }
+                }
+            });
         }
     }
 }
